3b49067ba8c2fe9cd92e0888a6fdd96520fa1c3a
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-04-18 18:52:54 -0300

    styles para 404

--- src/ram/pages/NotFound.jsx
+++ src/ram/pages/NotFound.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../../auth/components/styles-components.css"
 
 export const NotFound = () => {
   return (
-    <div>
-      <h1>Error 404</h1>
+    <div className='contenedor404'>
+      <h1 >Error 404</h1>
       <p>Página no encontrada</p>
     </div>
   )
