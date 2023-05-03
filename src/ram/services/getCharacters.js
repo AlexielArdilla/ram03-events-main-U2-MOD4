7f33c6ed12841d68528dd44d78d2a01770fafab5
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-05-03 00:39:56 -0300

    Update getCharacters.js

--- src/ram/services/getCharacters.js
+++ src/ram/services/getCharacters.js
@@ -5,3 +5,12 @@ export function getCharacters(){
       .then((res) => res.json())
 }
 
+export function getLocations(){
+  return fetch("https://rickandmortyapi.com/api/location")
+  .then((res) => res.json())
+}
+
+export function getEpisodes(){
+  return fetch("https://rickandmortyapi.com/api/episode")
+  .then((res) => res.json())
+}
